e5622b92fa99ae5c53448772c124991891434734
Sean Kim <sean@fairlabs.io>
2023-12-26 10:35:52 +0900

    update

--- App.js
+++ App.js
@@ -1,13 +1,85 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NotifierWrapper } from "react-native-notifier";
+import { Button, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+
+const Link = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Button
+      title="Click"
+      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+    />
+  );
+};
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+  </Screen>
+);
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>TweetDetails {route.params.id}</Text>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+const FeedNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={({ route }) => ({
+        title: `${route.params.id}`,
+        headerStyle: { backgroundColor: "tomato" },
+      })}
+    />
+  </Stack.Navigator>
+);
+
+const Account = () => (
+  <Screen>
+    <Text>Account</Text>
+  </Screen>
+);
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveTintColor: "#e91e63",
+    }}
+  >
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+);
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NotifierWrapper>
-        <ListingEditScreen />
+        <NavigationContainer>
+          <TabNavigator />
+        </NavigationContainer>
       </NotifierWrapper>
     </GestureHandlerRootView>
   );
