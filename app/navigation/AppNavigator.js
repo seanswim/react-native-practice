45fbd676bb6bb4b727e59de74201f6d55d341bca
Sean Kim <sean@fairlabs.io>
2024-01-10 17:45:26 +0900

    update

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
 
 import ListingEditScreen from "../screens/ListingEditScreen";
+import expoPushTokensApi from "../api/expoPushTokens";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
 import NewListingButton from "./NewListingButton";
 
 const Tab = createBottomTabNavigator();
 
-function AppNavigator(props) {
+const AppNavigator = () => {
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+
+    Notifications.addNotificationReceivedListener((notification) =>
+      console.log(notification)
+    );
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig.extra.eas.projectId,
+      });
+      expoPushTokensApi.register(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    return token.data;
+  }
+
   return (
     <Tab.Navigator>
       <Tab.Screen
@@ -52,6 +98,6 @@ function AppNavigator(props) {
       />
     </Tab.Navigator>
   );
-}
+};
 
 export default AppNavigator;
